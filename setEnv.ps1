262e9013b760e58526a03a77a9e270262b138d61
Shawn Cicoria <github@cicoria.com>
2020-11-11 10:48:46 -0500

    runnig OK

--- setEnv.ps1
+++ setEnv.ps1
@@ -1,4 +1,14 @@
 
 $env:PATH="E:\g\bin;E:\CUDA\CUDA101Dev\bin;E:\CUDA\CUDA101Dev\extras\CUPTI\libx64;E:\CUDA\CUDA101Dev\include;$env:PATH"
 $env:PYTHONPATH=$(Get-Location)
+
+$env:PIPENV_IGNORE_VIRTUALENVS="yes"
+$env:PIPENV_VENV_IN_PROJECT="yes"
+$env:POETRY_VIRTUALENVS_IN_PROJECT="true"
+$env:POETRY_VIRTUALENVS_CREATE="true"
+
+$env:JUPYTER_CONFIG_DIR="${PWD}\.jupyter\config"
+$env:JUPYTER_RUNTIME_DIR="${PWD}\.jupyter\runtime"
+$env:JUPYTER_DATA_DIR="${PWD}\.jupyter\data"
+
 .\py369A\Scripts\Activate.ps1
