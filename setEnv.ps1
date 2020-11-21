fe83eb7e86a9149fcadc4b1a1fe3a83fbe8721e7
Shawn Cicoria <github@cicoria.com>
2020-11-21 14:06:33 -0500

    notebook cleanup

--- setEnv.ps1
+++ setEnv.ps1
@@ -1,5 +1,4 @@
-
-$env:PATH="E:\g\bin;E:\CUDA\CUDA101Dev\bin;E:\CUDA\CUDA101Dev\extras\CUPTI\libx64;E:\CUDA\CUDA101Dev\include;$env:PATH"
+$env:PATH="C:\g\bin;C:\CUDA\CUDA101Dev\bin;C:\CUDA\CUDA101Dev\extras\CUPTI\libx64;C:\CUDA\CUDA101Dev\include;$env:PATH"
 $env:PYTHONPATH=$(Get-Location)
 
 $env:PIPENV_IGNORE_VIRTUALENVS="yes"
@@ -11,4 +10,7 @@ $env:JUPYTER_CONFIG_DIR="${PWD}\.jupyter\config"
 $env:JUPYTER_RUNTIME_DIR="${PWD}\.jupyter\runtime"
 $env:JUPYTER_DATA_DIR="${PWD}\.jupyter\data"
 
-.\py369A\Scripts\Activate.ps1
+# this forces NO GPU - deal with the OOM issues.
+$env:CUDA_VISIBLE_DEVICES="-1"
+
+.venv\Scripts\Activate.ps1
